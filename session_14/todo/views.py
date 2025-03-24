e6685b527b3cc801e8924a75857b013b21efde8d
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2025-03-24 22:05:09 +0000

    Add files via upload

--- session_14/todo/views.py
+++ session_14/todo/views.py
@@ -1,22 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseForbidden
-from django.views.generic import  ListView, TemplateView, FormView
+from django.views.generic import  ListView, TemplateView, FormView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import get_user
+from django.contrib.auth import get_user, logout
 from django.contrib.auth.forms import UserCreationForm
 
+from todo.forms import TasksForm
 from todo.models import Task
 
 # def index(request):
 #     tasks = Task.objects.all()
 #     return render(request,"todo/index.html", {"foo": "cenas", "tasks": tasks})
 
-class TaskListView(LoginRequiredMixin, ListView):
+class TaskListView(LoginRequiredMixin, CreateView):
     login_url = "/signin"
-    model = Task
+    success_url = "/tasks"
+    form_class = TasksForm
+    template_name = "todo/task_list.html"
 
-    def get_queryset(self):
-        return Task.objects.filter(user=self.request.user).all()
+    def get_context_data(self, **kwargs):
+        kwargs['object_list'] = Task.objects.filter(user=self.request.user).all()
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class IndexView(TemplateView):
     http_method_names = ['get']
@@ -30,3 +38,8 @@ class SingUpView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/")
