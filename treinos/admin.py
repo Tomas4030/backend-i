ae2393522b0dd3bff55d843bd6912fcd70db3864
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2025-03-31 02:18:39 +0100

    Delete treinos directory

--- treinos/admin.py
+++
@@ -1,9 +0,0 @@
- # admin.py
-from django.contrib import admin
-from .models import GymProfile
-
-class GymProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'altura', 'peso', 'objetivos', 'dias_treino')  
-    search_fields = ('user__username',)  
-
-admin.site.register(GymProfile, GymProfileAdmin)
